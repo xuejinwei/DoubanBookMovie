c06c4d15742cae957030d55f656c2135f8afaef8
xuejinwei <xuejinwei@outlook.com>
2016-04-07 16:53:03 +0800

    修复git大小写敏感

--- app/src/main/java/com/xuejinwei/doubanbookmovie/doubanbookmovie/api/api.java
+++ app/src/main/java/com/xuejinwei/doubanbookmovie/doubanbookmovie/api/api.java
@@ -1,10 +1,13 @@
 package com.xuejinwei.doubanbookmovie.doubanbookmovie.api;
 
+import com.xuejinwei.doubanbookmovie.doubanbookmovie.model.Celebrity;
+import com.xuejinwei.doubanbookmovie.doubanbookmovie.model.HtmlResult;
 import com.xuejinwei.doubanbookmovie.doubanbookmovie.model.Movie;
 import com.xuejinwei.doubanbookmovie.doubanbookmovie.model.MovieResult;
 import com.xuejinwei.doubanbookmovie.doubanbookmovie.model.Result;
 
 import retrofit2.http.GET;
+import retrofit2.http.Header;
 import retrofit2.http.Path;
 import retrofit2.http.Query;
 import rx.Observable;
@@ -16,33 +19,43 @@ public interface Api {
 
     /**
      * 查询正在热映movie
-     * @param start  int 开始数
+     *
+     * @param start int 开始数
      * @param count 查询数量，传入0 为默认数量
      * @return
      */
-    @GET("/v2/movie/in_theaters")
+    @GET("movie/in_theaters")
     Observable<Result<MovieResult>> getMovieInTheaters(@Query("start") int start, @Query("count") int count);
 
     /**
      * 查询即将上映映movie
      *
-     * @param start  int 开始数
+     * @param start int 开始数
      * @param count 查询数量，传入0 为默认数量
      * @return
      */
-    @GET("/v2/movie/coming_soon")
+    @GET("movie/coming_soon")
     Observable<Result<MovieResult>> getMovieComingSoon(@Query("start") int start, @Query("count") int count);
 
     /**
      * 查询TOP250
      *
-     * @param start  int 开始数
+     * @param start int 开始数
      * @param count 查询数量，传入0 为默认数量
      * @return
      */
-    @GET("/v2/movie/top250")
+    @GET("movie/top250")
     Observable<Result<MovieResult>> getMovieTop250(@Query("start") int start, @Query("count") int count);
 
-    @GET("/v2/movie/subject/{id}")
+    @GET("movie/subject/{id}")
     Observable<Result<Movie>> getMovieById(@Path("id") String id);
+
+    @GET("movie/celebrity/{id}")
+    Observable<Result<Celebrity>> getCelebrityDetail(@Path("id") String id);
+
+    @GET("https://movie.douban.com/subject/24750534/comments?start=0&limit=20&sort=new_score")
+    Observable<HtmlResult> getComment();
+
+    @GET("book/25843319/collection")
+    Observable<Result<String>> getCollectionDetail(@Header("Authorization") String Authorization);
 }
