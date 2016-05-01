cab7b4ac3b05f6c040421b70a7bf4dcd66a62b0d
xuejinwei <xuejinwei@outlook.com>
2016-05-01 10:02:27 +0800

    修改图书搜藏UI ，fixbug，如果rating为空，其他不显示的问题

--- app/src/main/java/com/xuejinwei/doubanbookmovie/doubanbookmovie/ui/activity/BookCollectionDetailEditActivity.java
+++ app/src/main/java/com/xuejinwei/doubanbookmovie/doubanbookmovie/ui/activity/BookCollectionDetailEditActivity.java
@@ -2,11 +2,12 @@ package com.xuejinwei.doubanbookmovie.doubanbookmovie.ui.activity;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.LayerDrawable;
 import android.os.Bundle;
-import android.support.v7.widget.AppCompatRatingBar;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.Toolbar;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.RatingBar;
@@ -16,6 +17,8 @@ import com.xuejinwei.doubanbookmovie.doubanbookmovie.R;
 import com.xuejinwei.doubanbookmovie.doubanbookmovie.model.CollectionUpdate;
 import com.xuejinwei.doubanbookmovie.doubanbookmovie.ui.base.activity.SwipeBackActivity;
 import com.xuejinwei.doubanbookmovie.doubanbookmovie.util.CommonUtil;
+import com.xuejinwei.doubanbookmovie.doubanbookmovie.util.StringUtils;
+import com.xuejinwei.doubanbookmovie.doubanbookmovie.widget.CountEditText;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -27,18 +30,18 @@ import butterknife.ButterKnife;
 public class BookCollectionDetailEditActivity extends SwipeBackActivity {
     public static final String ARG_TYPE = "type";
     public static final String BOOK_ID  = "book_id";
+    @Bind(R.id.toolbar)      Toolbar       toolbar;
+    @Bind(R.id.ratingBar)    RatingBar     ratingBar;
+    @Bind(R.id.tv_rating)    TextView      tv_rating;
+    @Bind(R.id.count_desc)   CountEditText count_desc;
+    @Bind(R.id.radioButton1) RadioButton   radioButton1;
+    @Bind(R.id.radioButton2) RadioButton   radioButton2;
+    @Bind(R.id.radioButton3) RadioButton   radioButton3;
+    @Bind(R.id.radioGroup)   RadioGroup    radioGroup;
+    @Bind(R.id.btn_submit)   Button        btn_submit;
 
     private CollectionUpdate collectionUpdate;
 
-    @Bind(R.id.et_comment)   EditText           et_comment;
-    @Bind(R.id.radioButton1) RadioButton        radioButton1;
-    @Bind(R.id.radioButton2) RadioButton        radioButton2;
-    @Bind(R.id.radioButton3) RadioButton        radioButton3;
-    @Bind(R.id.radioGroup)   RadioGroup         radioGroup;
-    @Bind(R.id.ratingBar)    AppCompatRatingBar ratingBar;
-    @Bind(R.id.tv_rating)    TextView           tv_rating;
-    @Bind(R.id.toolbar)      Toolbar            toolbar;
-    @Bind(R.id.btn_submit)   Button             btn_submit;
 
     public enum Type {
         ADD,      // 添加
@@ -52,7 +55,7 @@ public class BookCollectionDetailEditActivity extends SwipeBackActivity {
         Intent starter = new Intent(context, BookCollectionDetailEditActivity.class);
         starter.putExtra(ARG_TYPE, type);
         starter.putExtra(BOOK_ID, book_id);
-        context.startActivityForResult(starter,0);
+        context.startActivityForResult(starter, 0);
     }
 
     @Override
@@ -63,41 +66,50 @@ public class BookCollectionDetailEditActivity extends SwipeBackActivity {
         mType = (Type) getIntent().getSerializableExtra(ARG_TYPE);
         book_id = getIntent().getStringExtra(BOOK_ID);
         collectionUpdate = new CollectionUpdate();
+        ratingBar.setOnRatingBarChangeListener((bar, rating, fromUser) -> {
+            int score = (int) rating;
+            updateRatingText(score);
+        });
+
         if (mType == Type.EDIT) {
             setTitle("编辑搜藏");
             runRxTaskOnUi(mApiWrapper.getBookCollectionsDetail(book_id), bookCollections -> {
                 collectionUpdate.comment = bookCollections.comment;
                 collectionUpdate.status = bookCollections.status;
-                collectionUpdate.rating = String.valueOf(bookCollections.rating.value);
 
-                et_comment.setText(bookCollections.comment);
-                ratingBar.setRating(Float.valueOf(String.valueOf(bookCollections.rating.value)));
-                tv_rating.setText(String.valueOf(bookCollections.rating.value));
+                count_desc.setText(bookCollections.comment);
+                if (bookCollections.rating != null) {
+                    collectionUpdate.rating = String.valueOf(bookCollections.rating.value);
+                    ratingBar.setRating(Float.valueOf(String.valueOf(bookCollections.rating.value)));
+                }
                 if (bookCollections.status.equals("wish")) {
                     radioButton1.setChecked(true);
                 }
                 if (bookCollections.status.equals("reading")) {
                     radioButton2.setChecked(true);
                 }
-                if (bookCollections.status.equals("doing")) {
-                    radioButton2.setChecked(true);
+                if (bookCollections.status.equals("read")) {
+                    radioButton3.setChecked(true);
                 }
             });
         } else if (mType == Type.ADD) {
             setTitle("添加搜藏");
+            ratingBar.setRating(3);
         } else {
             CommonUtil.toast("类型错误");
             finish();
         }
-        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
-            @Override
-            public void onRatingChanged(RatingBar ratingBar1, float rating, boolean fromUser) {
-//                tv_rating.setText((int) rating);
-            }
-        });
         btn_submit.setOnClickListener(v -> {
-            collectionUpdate.comment = et_comment.getText().toString();
-            collectionUpdate.rating = String.valueOf((int) ratingBar.getRating());
+            if (ratingBar.getRating() == 0) {
+                CommonUtil.toast("请设置一个评分");
+                return;
+            }
+            if (StringUtils.isEmpty(count_desc.getCountText().getText().toString())) {
+                CommonUtil.toast("请输入搜藏寄语");
+                return;
+            }
+            collectionUpdate.comment = count_desc.getEditText().getText().toString();
+            collectionUpdate.rating = String.valueOf(ratingBar.getRating());
             collectionUpdate.status = getStatus();
             if (mType == Type.EDIT) {
                 runRxTaskOnUi(mApiWrapper.updateBookCollections(book_id, collectionUpdate), bookCollections -> {
@@ -127,5 +139,21 @@ public class BookCollectionDetailEditActivity extends SwipeBackActivity {
             return "read";
         }
         return "wish";
+    }
+
+    public void updateRatingText(int score) {
+        String[] ratingDescription = {"请选择", "较差", "中等", "很好", "优秀", "极好"};
+        int colors[] = {
+                R.color.text_color_grey,
+                R.color.google_red,
+                R.color.text_color_grey_full,
+                R.color.md_green_500,
+                R.color.md_green_400,
+                R.color.md_green_300
+        };
+        LayerDrawable stars = (LayerDrawable) ratingBar.getProgressDrawable();
+        stars.getDrawable(2).setColorFilter(ContextCompat.getColor(this, colors[score]), PorterDuff.Mode.SRC_ATOP);
+        tv_rating.setTextColor(ContextCompat.getColor(this, colors[score]));
+        tv_rating.setText(ratingDescription[score]);
     }
 }
